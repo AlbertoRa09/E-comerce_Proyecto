6c5cf392f817b1fd7f0fa6612f10f423723bcd08
AlbertoRa09 <Xion507@gmail.com>
2025-04-11 19:07:02 -0500

    Registro-1

--- app/src/main/java/com/example/proyecto_dsw9/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_dsw9/MainActivity.kt
@@ -1,20 +1,58 @@
 package com.example.proyecto_dsw9
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var passwordVisible = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Navegar a la pantalla de registro
+
+        val textViewRegistro = findViewById<TextView>(R.id.registro)
+        textViewRegistro.setOnClickListener {
+            val intent = Intent(this, RegistroActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Mostrar/ocultar contraseña
+        val editTextPassword = findViewById<EditText>(R.id.editTextTextPassword)
+        val imageViewToggle = findViewById<ImageView>(R.id.imageViewToggle)
+
+        imageViewToggle.setOnClickListener {
+            passwordVisible = !passwordVisible
+
+            if (passwordVisible) {
+                editTextPassword.inputType =
+                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                imageViewToggle.setImageResource(R.drawable.abierto)
+            } else {
+                editTextPassword.inputType =
+                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                imageViewToggle.setImageResource(R.drawable.cerrado)
+            }
+
+            editTextPassword.setSelection(editTextPassword.text.length)
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/proyecto_dsw9/RegistroActivity.kt
@@ -0,0 +1,45 @@
+package com.example.proyecto_dsw9
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.InputType
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class RegistroActivity : AppCompatActivity() {
+    private var passwordVisible = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.registro)
+
+        // Mostrar/ocultar contraseña Login
+        val editTextPassword1 = findViewById<EditText>(R.id.editTextTextPassword1)
+        val imageViewToggle1 = findViewById<ImageView>(R.id.imageViewToggle1)
+
+        imageViewToggle1.setOnClickListener {
+            passwordVisible = !passwordVisible
+
+            if (passwordVisible) {
+                editTextPassword1.inputType =
+                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                imageViewToggle1.setImageResource(R.drawable.abierto)
+            } else {
+                editTextPassword1.inputType =
+                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                imageViewToggle1.setImageResource(R.drawable.cerrado)
+            }
+
+            editTextPassword1.setSelection(editTextPassword1.text.length)
+        }
+
+        // Navegar a la pantalla de login
+
+        val textViewLogin = findViewById<TextView>(R.id.login)
+        textViewLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
